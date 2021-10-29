6397540a13557de83a76ac029e4632b346bc8c99
Tian Khamez <tkhamez@gmail.com>
2021-10-29 21:57:34 +0200

    Update to v0.3.6

--- frontend/static/js/40.18a903a1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@ping-board/frontend"]=this["webpackJsonp@ping-board/frontend"]||[]).push([[40],{188:function(e,n,r){e.exports=function(){"use strict";return{name:"en",weekdays:"Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday".split("_"),months:"January_February_March_April_May_June_July_August_September_October_November_December".split("_")}}()}}]);
-//# sourceMappingURL=40.18a903a1.chunk.js.map
